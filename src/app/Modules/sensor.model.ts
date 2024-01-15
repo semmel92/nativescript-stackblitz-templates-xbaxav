10ca35a77d83378cb37b52e12adc22931a67c4a1
semmel92 <2210859038@fh-burgenland.at>
2024-01-15 13:57:34 +0100

    Tesyt2

---
+++ src/app/Modules/sensor.model.ts
@@ -0,0 +1,14 @@
+export interface Sensor {
+    sensorId: number;
+    name: string;
+    location: string;
+    active: boolean;
+    type: SensorType;
+  }
+  
+  export enum SensorType {
+    OUTDOOR,
+    INDOOR,
+    WATER
+  }
+  
--- src/app/Services/backend.service.ts
+++ src/app/Services/backend.service.ts
@@ -1,15 +1,13 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Sensor } from '../Modules/sensor.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
   private apiUrl = 'http://localhost:8080/rest-api';
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
 
   constructor(private http: HttpClient) { }
 
